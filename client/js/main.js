eed68b55096b7936fef6a0387495c2a4be705c7b
rajibhasan01 <hasan.rajib1996@gmail.com>
2022-04-05 12:28:31 +0600

    updated

---
+++ client/js/main.js
@@ -0,0 +1,32 @@
+const form = document.querySelector("form"),
+fileInput = document.querySelector(".file-input"),
+progressArea = document.querySelector(".progress-area"),
+uploadedArea = document.querySelector(".uploaded-area");
+
+form.addEventListener("click", () =>{
+  fileInput.click();
+});
+
+fileInput.onchange = ({target})=>{
+  let file = target.files[0];
+  if(file){
+    uploadFile(file);
+  }
+}
+
+const uploadFile = (file) =>{
+    const formData = new FormData()
+    formData.append('image', file)
+
+    fetch(`http://localhost:5000/img/blur`, {
+        method: 'POST',
+        body: formData
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+      })
+      .catch(error => {
+        console.error(error)
+      })  
+}
