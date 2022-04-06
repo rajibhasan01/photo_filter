e5d1306be7bd45328c666d43fe9efe336027f922
rajibhasan01 <hasan.rajib1996@gmail.com>
2022-04-06 18:06:54 +0600

    updated

--- client/js/main.js
+++ client/js/main.js
@@ -5,9 +5,14 @@ uploadedArea = document.querySelector(".uploaded-area"),
 image = document.getElementById('image'),
 range = document.getElementById('range'),
 inside = document.getElementById('inside'),
-outside = document.getElementById('outside');
+outside = document.getElementById('outside'),
+input_div = document.getElementById("input-div"),
+tint_div = document.getElementById('tint'),
+gray_div = document.getElementById('gray');
+
 let value = 0;
 let imgPath = null;
+let red_value = 0, blue_value = 0, green_value = 0;
 
 
 // file upload
@@ -51,6 +56,27 @@ function myFunction(val) {
 }
 
 
+// range calculate
+function redValue(val) {
+  red_value = val;
+  document.getElementById("red").innerHTML = val;
+  
+}
+
+// range calculate
+function blueValue(val) {
+  blue_value = val;
+  document.getElementById("blue").innerHTML = val;
+  
+}
+
+// range calculate
+function greenValue(val) {
+  green_value = val;
+  document.getElementById("green").innerHTML = val;
+  
+}
+
 document.getElementById('range').addEventListener("change", () => {
   value = parseInt(value);
   let inside = 0;
@@ -84,7 +110,10 @@ document.getElementById('range').addEventListener("change", () => {
 
 
 document.getElementById('gray').addEventListener('click', ()=>{
-  const gray_data = {imgPath,grayscale:true}
+  const gray_data = {imgPath,grayscale:true};
+  input_div.classList.add('display_hidden');
+  tint_div.classList.remove('active');
+  gray_div.classList.add('active');
   fetch(`http://localhost:5000/img/gray`, {
         method: 'POST',
         headers: {
@@ -102,3 +131,58 @@ document.getElementById('gray').addEventListener('click', ()=>{
         console.error(error)
       }) 
 })
+
+
+// Tint api call
+const tintFilterApi = (tint_data) =>{
+  fetch(`http://localhost:5000/img/tint`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(tint_data)
+      })
+      .then(response => response.json())
+      .then(data => {
+        imgPath = data.imgPath;
+        image.src=`http://localhost:5000/${imgPath}?t=` + new Date().getTime();
+        console.log(imgPath)
+      })
+      .catch(error => {
+        console.error(error)
+      }) 
+}
+
+document.getElementById('tint').addEventListener('click', ()=>{
+  const color=[255,0,1];
+  const tint_data = {imgPath,color};
+  input_div.classList.remove("display_hidden");
+  tint_div.classList.add('active');
+  gray_div.classList.remove('active');
+  tintFilterApi(tint_data);
+})
+
+
+document.getElementById('green_color').addEventListener("change", () => {
+  const color = [red_value,blue_value,green_value];
+
+  const tint_data = {imgPath,color};
+  tintFilterApi(tint_data);
+  
+});
+
+document.getElementById('blue_color').addEventListener("change", () => {
+  const color = [red_value,blue_value,green_value];
+
+  const tint_data = {imgPath,color};
+  tintFilterApi(tint_data);
+  
+});
+
+document.getElementById('red_color').addEventListener("change", () => {
+  const color = [red_value,blue_value,green_value];
+
+  const tint_data = {imgPath,color};
+  tintFilterApi(tint_data);
+  
+});
