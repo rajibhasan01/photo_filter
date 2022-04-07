64939c5b2a0b68117386fe2c3d54297cd05451af
rajibhasan01 <hasan.rajib1996@gmail.com>
2022-04-07 16:51:37 +0600

    updated

--- client/js/main.js
+++ client/js/main.js
@@ -9,20 +9,26 @@ const form = document.querySelector("form"),
   input_div = document.getElementById("input-div"),
   tint_div = document.getElementById("tint"),
   gray_div = document.getElementById("gray");
-border_button = document.getElementById("border"),
+  border_button = document.getElementById("border"),
   border_range_div = document.getElementById("border_range_div"),
   image_container_div = document.getElementById("image-container"),
   img_1 = document.getElementById("img_1"),
   img_2 = document.getElementById("img_2"),
   img_3 = document.getElementById("img_3"),
   img_4 = document.getElementById("img_4"),
-  sharpen = document.getElementById("sharpen");
+  sharpen = document.getElementById("sharpen"),
+  custom_range = document.getElementById("custom-input-range"),
+  custom_btn = document.getElementById("custom");
 
 let value = 0;
 let imgPath = null;
 let red_value = 0,
   blue_value = 0,
-  green_value = 0;
+  green_value = 0,
+  a1 = 70, a2 = 67, a3 = 34,
+  b1 = 33, b2 = 45, b3 = 25,
+  c1 = 59, c2 = 65, c3 = 39;
+
 
 // file upload
 form.addEventListener("click", () => {
@@ -93,6 +99,63 @@ function greenValue(val) {
   document.getElementById("green").innerHTML = val;
 }
 
+// range calculate
+function a1Value(val) {
+  a1 = val;
+  document.getElementById("a1").innerHTML = val/100;
+}
+
+// range calculate
+function a2Value(val) {
+  a2 = val;
+  document.getElementById("a2").innerHTML = val/100;
+}
+
+// range calculate
+function a3Value(val) {
+  a3 = val;
+  document.getElementById("a3").innerHTML = val/100;
+}
+
+
+// range calculate
+function b1Value(val) {
+  b1 = val;
+  document.getElementById("b1").innerHTML = val/100;
+}
+
+// range calculate
+function b2Value(val) {
+  b2 = val;
+  document.getElementById("b2").innerHTML = val/100;
+}
+
+// range calculate
+function b3Value(val) {
+  b3 = val;
+  document.getElementById("b3").innerHTML = val/100;
+}
+
+// range calculate
+function c1Value(val) {
+  c1 = val;
+  document.getElementById("c1").innerHTML = val/100;
+}
+
+// range calculate
+function c2Value(val) {
+  c2 = val;
+  document.getElementById("c2").innerHTML = val/100;
+}
+
+// range calculate
+function c3Value(val) {
+  c3 = val;
+  document.getElementById("c3").innerHTML = val/100;
+}
+
+
+
 border_button.addEventListener("click", () => {
   border_range_div.classList.remove("display_hidden");
   border_button.classList.add("active");
@@ -100,6 +163,8 @@ border_button.addEventListener("click", () => {
   tint_div.classList.remove("active");
   gray_div.classList.remove("active");
   sharpen.classList.remove("active");
+  custom_range.classList.add('display_hidden');
+  custom_btn.classList.remove("active");
 
   let top = 15,
     bottom = 15,
@@ -161,6 +226,9 @@ document.getElementById("gray").addEventListener("click", () => {
   sharpen.classList.remove("active");
   border_button.classList.remove("active");
   border_range_div.classList.add("display_hidden");
+  custom_range.classList.add('display_hidden');
+  custom_btn.classList.remove("active");
+
   if (imgPath) {
     fetch(`http://localhost:5000/img/gray`, {
       method: "POST",
@@ -214,6 +282,9 @@ document.getElementById("tint").addEventListener("click", () => {
   sharpen.classList.remove("active");
   border_button.classList.remove("active");
   border_range_div.classList.add("display_hidden");
+  custom_range.classList.add('display_hidden');
+  custom_btn.classList.remove("active");
+
   tintFilterApi(tint_data);
 });
 
@@ -244,6 +315,9 @@ sharpen.addEventListener('click', ()=>{
   gray_div.classList.remove("active");
   border_button.classList.remove("active");
   border_range_div.classList.add("display_hidden");
+  custom_range.classList.add('display_hidden');
+  input_div.classList.add("display_hidden");
+  custom_btn.classList.remove("active");
   const sharpen_data = {
     imgPath,
     sigma: 3,
@@ -267,6 +341,119 @@ const sharpenApi = (sharpen_data) =>{
         "Content-Type": "application/json",
       },
       body: JSON.stringify(sharpen_data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        imgPath = data.imgPath;
+        image.src =
+          `http://localhost:5000/${imgPath}?t=` + new Date().getTime();
+        console.log(imgPath);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+};
+
+custom_btn.addEventListener('click', ()=>{
+  custom_range.classList.remove('display_hidden');
+  sharpen.classList.remove('active');
+  tint_div.classList.remove("active");
+  gray_div.classList.remove("active");
+  border_button.classList.remove("active");
+  border_range_div.classList.add("display_hidden");
+  input_div.classList.add("display_hidden");
+  custom_btn.classList.add("active");
+});
+
+
+document.getElementById("a1_value").addEventListener("change", () => {
+  let a = [a1/100, a2/100, a3/100], b = [b1/100, b2/100, b3/100], c = [c1/100, c2/100, c3/100];
+
+  
+  custom_data = {imgPath,a,b,c};
+  customFilterApi(custom_data );
+  
+});
+
+
+document.getElementById("a2_value").addEventListener("change", () => {
+  let a = [a1/100, a2/100, a3/100], b = [b1/100, b2/100, b3/100], c = [c1/100, c2/100, c3/100];
+
+  
+  custom_data = {imgPath,a,b,c};
+  customFilterApi(custom_data );
+});
+
+
+document.getElementById("a3_value").addEventListener("change", () => {
+  let a = [a1/100, a2/100, a3/100], b = [b1/100, b2/100, b3/100], c = [c1/100, c2/100, c3/100];
+
+  
+  custom_data = {imgPath,a,b,c};
+  customFilterApi(custom_data );
+});
+
+document.getElementById("b1_value").addEventListener("change", () => {
+  let a = [a1/100, a2/100, a3/100], b = [b1/100, b2/100, b3/100], c = [c1/100, c2/100, c3/100];
+
+  
+  custom_data = {imgPath,a,b,c};
+  customFilterApi(custom_data );
+});
+
+document.getElementById("b2_value").addEventListener("change", () => {
+  let a = [a1/100, a2/100, a3/100], b = [b1/100, b2/100, b3/100], c = [c1/100, c2/100, c3/100];
+
+  
+  custom_data = {imgPath,a,b,c};
+  customFilterApi(custom_data );
+});
+
+
+document.getElementById("b3_value").addEventListener("change", () => {
+  let a = [a1/100, a2/100, a3/100], b = [b1/100, b2/100, b3/100], c = [c1/100, c2/100, c3/100];
+
+  
+  custom_data = {imgPath,a,b,c};
+  customFilterApi(custom_data );
+});
+
+
+document.getElementById("c1_value").addEventListener("change", () => {
+  let a = [a1/100, a2/100, a3/100], b = [b1/100, b2/100, b3/100], c = [c1/100, c2/100, c3/100];
+
+  
+  custom_data = {imgPath,a,b,c};
+  customFilterApi(custom_data );
+});
+
+document.getElementById("c2_value").addEventListener("change", () => {
+  let a = [a1/100, a2/100, a3/100], b = [b1/100, b2/100, b3/100], c = [c1/100, c2/100, c3/100];
+
+  
+  custom_data = {imgPath,a,b,c};
+  customFilterApi(custom_data );
+});
+
+document.getElementById("c3_value").addEventListener("change", () => {
+  let a = [a1/100, a2/100, a3/100], b = [b1/100, b2/100, b3/100], c = [c1/100, c2/100, c3/100];
+
+  
+  custom_data = {imgPath,a,b,c};
+  customFilterApi(custom_data );
+});
+
+
+const customFilterApi = (custom_data) =>{
+  console.log('Got Hit')
+  if (custom_data.imgPath) {
+    fetch(`http://localhost:5000/img/custom_filter`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(custom_data),
     })
       .then((response) => response.json())
       .then((data) => {
